a698453e91706eb3fa193e13094ee4c49307b434
Sherwin <mazarellosherwin4@gmail.com>
2025-11-17 15:27:29 +0530

    Fourth commit

--- prac2.R
+++ prac2.R
@@ -8,10 +8,37 @@ print(int1)
 
 ##Create empty Df various colm types
 
-df <- data.frame(col1= numeric(),
-                 col2=integer(),
-                 col3=complex(),
-                 col4=logical(),
-                 col5=factor(),
-                 StringAsFactor ='FALSE')
-print(str(df))
+
+df <- data.frame(
+  col1 = numeric(0),
+  col2 = integer(0),
+  col3 = complex(0),
+  col4 = logical(0),
+  col5 = factor(character(0)),   # <-- FIXED
+  stringsAsFactors = FALSE
+)
+
+str(df)
+
+name =c("sherwin","allan","ervin")
+mrk =c(10,20,30)
+attempts=c(1,2,1)
+quality=c("yes","no","yes")
+
+df <- data.frame( Name=name,
+                  marks=mrk,
+                  attempts =attempts,
+                  Quality =quality
+                  )
+print(df)
+## to add a column basicallt
+df $ status = c('M', 'U', 'M')
+print(df)
+df $ Gender =c("M","M","F")
+print(df)
+
+## addin a nw column 
+df1 <- data.frame(Name="Angad",marks=34,attempts=3,Quality="no",status="M",Gender="M")
+df <- rbind(df1,df)
+print(df)
+
